fc395a3d7da5fd456c9a82e1b3d642e086bf0051
02349 <jason.huangyu@gcnc-group.com>
2020-06-23 17:27:23 +0800

    no message

--- cicd/Properties/AssemblyInfo.cs
+++ cicd/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("261bf08d-3bf5-4392-b618-970bc9e704e5")]
+[assembly: Guid("a5774eb0-54e7-48b8-acf8-0324406fc079")]
 
 // 組件的版本資訊是由下列四項值構成:
 //
